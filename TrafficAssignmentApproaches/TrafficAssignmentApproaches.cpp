003f9c1ffdd73010e07b7d2f39fa3acdadb215fd
baspresso <baspresso@gmail.com>
2024-09-15 20:40:38 +0300

    Add project files.

---
+++ TrafficAssignmentApproaches/TrafficAssignmentApproaches.cpp
@@ -0,0 +1,12 @@
+﻿// TrafficAssignmentApproaches.cpp : Defines the entry point for the application.
+//
+
+#include "TrafficAssignmentApproaches.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
