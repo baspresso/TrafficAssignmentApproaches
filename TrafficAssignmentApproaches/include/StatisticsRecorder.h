04dea9cbc3d0b8243473d3bfb1d88b25a8cdd76b
baspresso <baspresso@gmail.com>
2024-09-28 13:31:23 +0300

    Gitignore change

--- TrafficAssignmentApproaches/include/StatisticsRecorder.h
+++ TrafficAssignmentApproaches/include/StatisticsRecorder.h
@@ -1,4 +1,14 @@
 #ifndef STATISTICS_RECORDER_H
 #define STATISTICS_RECORDER_H
 
+namespace TrafficAssignment {
+  class StatisticsRecorder {
+    StatisticsRecorder() {
+
+    }
+
+    ~StatisticsRecorder() = default;
+  };
+}
+
 #endif STATISTICS_RECORDER_H
