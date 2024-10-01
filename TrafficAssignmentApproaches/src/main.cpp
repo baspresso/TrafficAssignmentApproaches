7ea472c58c6bd1ae2abd257ce9d038af30facebd
baspresso <baspresso@gmail.com>
2024-10-01 20:32:56 +0300

    Added TapasAdvancedGradientDescentAppoach

--- TrafficAssignmentApproaches/src/main.cpp
+++ TrafficAssignmentApproaches/src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "RouteBasedKrylatov2023Approach.h"
 #include "TapasLineSearchApproach.h"
+#include "TapasAdvancedGradientDescentAppoach.h"
 
 int main() {
-	TrafficAssignment::RouteBasedKrylatov2023Approach<long double>("Anaheim").ComputeTrafficFlows();
-	TrafficAssignment::TapasLineSearchApproach<long double>("Anaheim").ComputeTrafficFlows();
+	//TrafficAssignment::RouteBasedKrylatov2023Approach<long double>("Anaheim").ComputeTrafficFlows();
+	//TrafficAssignment::TapasLineSearchApproach<long double>("Anaheim").ComputeTrafficFlows();
+	TrafficAssignment::TapasAdvancedGradientDescentAppoach<long double>("SiouxFalls").ComputeTrafficFlows();
 	return 0;
 }
